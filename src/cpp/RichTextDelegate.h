2738bf34bf7d20e28f1fa16f14e719b564b524c1
Simon Edwards <simon@simonzone.com>
2022-06-06 16:54:34 +0200

    Fix code formatting; try to fix Win32 linking

--- src/cpp/RichTextDelegate.h
+++ src/cpp/RichTextDelegate.h
@@ -4,12 +4,14 @@
 #include <QTextDocument>
 
 
-class RichTextDelegate : public QStyledItemDelegate {
-    public:
-        using QStyledItemDelegate::QStyledItemDelegate;
-    protected:
-        void paint(QPainter * painter, const QStyleOptionViewItem & option, const QModelIndex & index) const;
-        QSize sizeHint(const QStyleOptionViewItem & option, const QModelIndex & index) const;
-    private:
-        void prepareTextDocument(QTextDocument &doc, QStyleOptionViewItem &option) const;
+class DLL_EXPORT RichTextDelegate : public QStyledItemDelegate {
+ public:
+  using QStyledItemDelegate::QStyledItemDelegate;
+
+ protected:
+  void paint(QPainter * painter, const QStyleOptionViewItem & option, const QModelIndex & index) const;
+  QSize sizeHint(const QStyleOptionViewItem & option, const QModelIndex & index) const;
+
+ private:
+  void prepareTextDocument(QTextDocument &doc, QStyleOptionViewItem &option) const;
 };
